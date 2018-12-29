b38fd039f0ac98c40b4c282e1ce517645f2133cb
sterkh <pmcc@yandex.ru>
2018-12-29 11:50:23 +0300

    Add complex message type

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -3,11 +3,15 @@ package com.example
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 
+
+case class ComplexMessage(name: String, ts: Int)
+
 //#greeter-companion
 //#greeter-messages
 object Greeter {
   //#greeter-messages
-  def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
+  def props(message: String, printerActor: ActorRef, complexMessage: Option[ComplexMessage] = None): Props =
+    Props(new Greeter(message, printerActor, complexMessage))
   //#greeter-messages
   final case class WhoToGreet(who: String)
   case object Greet
@@ -16,7 +20,7 @@ object Greeter {
 //#greeter-companion
 
 //#greeter-actor
-class Greeter(message: String, printerActor: ActorRef) extends Actor {
+class Greeter(message: String, printerActor: ActorRef, complexMessage: Option[ComplexMessage] = None ) extends Actor {
   import Greeter._
   import Printer._
 
@@ -28,7 +32,10 @@ class Greeter(message: String, printerActor: ActorRef) extends Actor {
     case Greet           =>
       //#greeter-send-message
       printerActor ! Greeting(greeting)
+      printerActor ! complexMessage.getOrElse("")
       //#greeter-send-message
+    case ComplexMessage(name, ts) =>
+      printerActor ! ComplexMessage(name, ts)
   }
 }
 //#greeter-actor
@@ -51,6 +58,8 @@ class Printer extends Actor with ActorLogging {
   def receive = {
     case Greeting(greeting) =>
       log.info("Greeting received (from " + sender() + "): " + greeting)
+    case ComplexMessage(name, ts) =>
+      log.info("Event received (from " + sender() + "): Event occured name = " + name + ", ts = " + ts)
   }
 }
 //#printer-actor
@@ -66,9 +75,11 @@ object AkkaQuickstart extends App {
   // Create the printer actor
   val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
 
+  val complexMessage = ComplexMessage("event1", 1234567891)
+
   // Create the 'greeter' actors
   val howdyGreeter: ActorRef =
-    system.actorOf(Greeter.props("Howdy", printer), "howdyGreeter")
+    system.actorOf(Greeter.props("Howdy", printer, Some(complexMessage)), "howdyGreeter")
   val helloGreeter: ActorRef =
     system.actorOf(Greeter.props("Hello", printer), "helloGreeter")
   val goodDayGreeter: ActorRef =
@@ -78,12 +89,15 @@ object AkkaQuickstart extends App {
   //#main-send-messages
   howdyGreeter ! WhoToGreet("Akka")
   howdyGreeter ! Greet
+  howdyGreeter ! ComplexMessage("event2", 1234567892)
 
   howdyGreeter ! WhoToGreet("Lightbend")
   howdyGreeter ! Greet
+  howdyGreeter ! ComplexMessage("event3", 1234567893)
 
   helloGreeter ! WhoToGreet("Scala")
   helloGreeter ! Greet
+  helloGreeter ! ComplexMessage("event4", 1234567894)
 
   goodDayGreeter ! WhoToGreet("Play")
   goodDayGreeter ! Greet
